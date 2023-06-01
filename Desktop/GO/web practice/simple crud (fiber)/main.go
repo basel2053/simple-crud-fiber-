ee630aad33977a5c8acfa3b75de78ff456ac1494
basel2053 <baselsalah2053@gmail.com>
2023-06-01 20:54:25 +0300

    adding air and testing statuscode and sending strings

--- Desktop/GO/web practice/simple crud (fiber)/main.go
+++ Desktop/GO/web practice/simple crud (fiber)/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
     app := fiber.New()
+
+		app.Post("/todos",func(c *fiber.Ctx) error {
+			result:= c.Body()
+			fmt.Println(result)
+			return c.SendString("Hey ")
+		})
+		
+		app.Get("/todos/:id",func(c *fiber.Ctx) error {
+			var id = c.Params("id")
+			return c.Status(fiber.StatusOK).SendString(id);
+		})
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World 👋!")
     })
 
-    app.Listen(":3000")
+    app.Listen("localhost:3000")
 }
